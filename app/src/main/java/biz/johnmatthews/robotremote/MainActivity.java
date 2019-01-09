dcc3b74a5e58a0034766c64d148d363261478e9e
johnmatthews7234 <john.matthews72@gmail.com>
2019-01-10 08:50:52 +1100

    Added initial layout

--- app/src/main/java/biz/johnmatthews/robotremote/MainActivity.java
+++ app/src/main/java/biz/johnmatthews/robotremote/MainActivity.java
@@ -2,6 +2,7 @@ package biz.johnmatthews.robotremote;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickStop(View view) {
+    }
+
+    public void onClickStart(View view) {
+    }
+
+    public void onClickSend(View view) {
+    }
+
+    public void onClickClear(View view) {
     }
 }
