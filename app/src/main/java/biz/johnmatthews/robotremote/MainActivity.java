6bed0ce7cd119424cbee9f8a405e4ffac3f7b01b
johnmatthews7234 <john.matthews72@gmail.com>
2019-01-10 11:38:28 +1100

    "Should" compile now.

--- app/src/main/java/biz/johnmatthews/robotremote/MainActivity.java
+++ app/src/main/java/biz/johnmatthews/robotremote/MainActivity.java
@@ -2,33 +2,137 @@ package biz.johnmatthews.robotremote;
 
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.hardware.usb.UsbDevice;
 import android.hardware.usb.UsbDeviceConnection;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.hardware.usb.UsbManager;
 import android.hardware.usb.UsbDevice;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
+import com.felhr.usbserial.UsbSerialDevice;
+import com.felhr.usbserial.UsbSerialInterface;
+
+import java.io.UnsupportedEncodingException;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MainActivity extends AppCompatActivity {
+
     public final String ACTION_USB_PERMISSION = "biz.johnmatthews.robotremote.USB_PERMISSION";
     UsbManager usbManager;
     UsbDevice device;
     UsbDeviceConnection deviceConnection;
+    UsbSerialDevice serialPort;
+    Button startButton;
+    Button sendButton;
+    Button stopButton;
+    Button clearButton;
+    EditText editText;
+    TextView textView;
+
+    UsbSerialInterface.UsbReadCallback mCallback = new UsbSerialInterface.UsbReadCallback() {
+        @Override
+        public void onReceivedData(byte[] bytes) {
+            String data = null;
+            try {
+                data = new String(bytes, "UTF-8");
+                data.concat("/n");
+                tvAppend(textView, data);
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            }
+        }
+    };
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(ACTION_USB_PERMISSION)) {
+                boolean granted = intent.getExtras().getBoolean(UsbManager.EXTRA_PERMISSION_GRANTED);
+                if (granted) {
+                    deviceConnection = usbManager.openDevice(device);
+                    serialPort = UsbSerialDevice.createUsbSerialDevice(device, deviceConnection);
+                    if (serialPort != null) {
+                        if (serialPort.open()) {
+                            setUiEnabled(true);
+                            serialPort.setBaudRate(9600);
+                            serialPort.setDataBits(UsbSerialInterface.DATA_BITS_8);
+                            serialPort.setStopBits(UsbSerialInterface.STOP_BITS_1);
+                            serialPort.setParity(UsbSerialInterface.PARITY_NONE);
+                            serialPort.setFlowControl(UsbSerialInterface.FLOW_CONTROL_OFF);
+                            serialPort.read(mCallback);
+                            tvAppend(textView, R.string.connection_opened + "\n");
+                        } else {
+                            Log.d("SERIAL", "PORT NOT OPEN");
+                        }
+                    } else {
+                        Log.d("SERIAL", "PORT IS NULL");
+                    }
+                } else {
+                        Log.d( "SERIAL", "PERMISSION NOT GRANTED");
+                }
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_ATTACHED)){
+                onClickStart(startButton);
+            } else if (intent.getAction().equals(UsbManager.ACTION_USB_DEVICE_DETACHED)) {
+                onClickStop(stopButton);
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usbManager = (UsbManager) getSystemService(this.USB_SERVICE);
+
+        startButton = findViewById(R.id.buttonStart);
+        sendButton = findViewById(R.id.buttonSend);
+        stopButton = findViewById(R.id.buttonStop);
+        clearButton = findViewById(R.id.buttonClear);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        setUiEnabled(false);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(ACTION_USB_PERMISSION);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_ATTACHED);
+        filter.addAction(UsbManager.ACTION_USB_DEVICE_DETACHED);
+        registerReceiver(broadcastReceiver, filter);
+    }
+
+    public void setUiEnabled(boolean bool) {
+       startButton.setEnabled(!bool);
+       sendButton.setEnabled(bool);
+       stopButton.setEnabled(bool);
+       textView.setEnabled(bool);
+    }
+
+    private void tvAppend(TextView tv, CharSequence text) {
+        final TextView ftv = tv;
+        final CharSequence ftext = text;
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                ftv.append(ftext);
+            }
+        });
+
     }
 
     public void onClickStop(View view) {
+        setUiEnabled(false);
+        serialPort.close();
+        tvAppend(textView, "\n" + R.string.connection_closed + "\n");
     }
 
     public void onClickStart(View view) {
@@ -55,6 +159,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onClickSend(View view) {
+        String string = editText.getText().toString();
+        serialPort.write(string.getBytes());
+        tvAppend(textView, "\n" + R.string.data_sent + string + "\n");
     }
 
     public void onClickClear(View view) {
